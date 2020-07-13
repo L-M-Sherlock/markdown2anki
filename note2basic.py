88a4a641a23a4a3d169de4108f3be79b4c3aed95
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-07-13 23:37:12 +0800

    对图片文件的支持

--- note2basic.py
+++ note2basic.py
@@ -1,9 +1,14 @@
 import re
 import utils
-
-
-note_path = "./Basic/近世代数note.md"
-with open(note_path, 'r', encoding='UTF-8') as note_file:
+def Basic(QA):
+    QA = QA.group()
+    QA = re.sub(r'\$\$(.*?)\$\$', utils.replace_formula, QA) #这里修改了
+    QA = re.sub(r'!\[(.*?)\]', utils.replace_img, QA)
+    QA = re.sub(r'\*\*(.*?)\*\*', utils.replace_Bold, QA)#加粗
+    return QA
+name="SoneSton" #需要修改为自己的
+notefile_path="..\\..\\MD\\线性代数.md"#需要修改为自己的
+with open(notefile_path, 'r', encoding='UTF-8') as note_file:
     cards = []
     lines = note_file.readlines()
     for line in lines:
@@ -12,16 +17,22 @@ with open(note_path, 'r', encoding='UTF-8') as note_file:
         elif '#' in line:
             title = line.replace('#', '')
             title = title.strip()
-        else:
-            Q = re.search(r'(?<=Q：).*?(?=A：)', line)
-            A = re.search(r'(?<=A：).*?(?=\n)', line)
+        else: 
+            #img 处理
+            imgPath=re.search(r'(?<=\]\().*?(?=\))', line)
+            if imgPath is not None:
+                imgPath=imgPath.group()
+                line=line.replace("]("+imgPath+")" ,"."+imgPath.split(".")[-1]+"]")#移除判断多余内容,并添加后缀
+                imgPath=utils.dirname(notefile_path)+imgPath.replace("/","\\")#取图片路径
+                utils.Anki_Saveimg(imgPath,name)#将图片拷贝到Anki媒体目录
+            #img 处理结束
+            Q = re.search(r'(?<=Q:).*?(?=A:)', line)
+            A = re.search(r'(?<=A:).*?(?=\n)', line)
             if Q is None or A is None:
                 continue
-            Q = Q.group()
-            A = A.group()
-            Q = re.sub(r'\$(.*?)\$', utils.replace_formula, Q)
-            A = re.sub(r'\$(.*?)\$', utils.replace_formula, A)
-            card = Q + '\t' + title + '\t' + A + '\n'
+            Q = Basic(Q)
+            A = Basic(A)
+            card = Q + '\t' + title + '\t' + A + '\t'+'\n'
             cards.append(card)
 
 with open('./Basic/anki_basic.txt', 'w', encoding='UTF-8') as txt_file:
